a71d1d7fd43bff25b54a6bd6de5aef82cd2d3502
Donald J. Fortier II <Technical_13@yahoo.com>
2023-02-28 15:12:48 +0000

    `/reply` and `/say` now log to console when used.  I'll make it log to a logging channel in the server at a later time.

--- commands/reply.js
+++ commands/reply.js
@@ -7,12 +7,20 @@ module.exports = {
     const { channel, options } = interaction;
     const msgID = interaction.options.getString( 'message-id' );
     const myResponse = interaction.options.getString( 'response' );
+    const strAuthorTag = interaction.user.tag;
 
-    channel.messages.fetch( msgID ).then( message => {
-      message.reply( myResponse );
-      interaction.editReply( { content: 'Responded!' } );
+    channel.messages.fetch( msgID ).then( async message => {
+      await message.reply( myResponse ).then( responded => {
+        interaction.editReply( { content: 'Responded!' } );
+        console.log( '%o requested me to reply to %o in `%o`#%o:\n\t%o',
+          strAuthorTag, message.author.tag, message.guild.name, message.channel.name, myResponse
+        );
+      } );
     } ).catch( noMessage => {
       interaction.editReply( { content: 'Unable to find message to repyly to.' } );
+      console.log( '%o requested me to reply to a message I couldn\'t find (#%o):\n\t%o',
+        strAuthorTag, msgID, myResponse
+      );
     } );
 	}
 }
--- commands/say.js
+++ commands/say.js
@@ -6,22 +6,27 @@ module.exports = {
     interaction.deferReply( { ephemeral: true } );
     const speakChannel = interaction.options.getChannel( 'channel' ) || interaction.channel;
     const mySaying = interaction.options.getString( 'saying' );
+    const strAuthorTag = interaction.user.tag;
 
     if ( mySaying ) {      
       return speakChannel.send( mySaying ).then( spoke => {
-          interaction.editReply( { content: 'Said the thing!' } );                    } ).catch( muted => {
-          switch ( muted.code ) {
-            case 50001 :
-              let noChan = '<#' + speakChannel + '>';
-              interaction.editReply( {
-                content: 'I do not have permission to send messages in ' + noChan
-              } );
-              break;
-            default:
-              console.error( 'Unable to speak: %o', muted );
-              interaction.editReply( { content: 'Couldn\'t say the thing!' } );
-          }
-        } );
+        interaction.editReply( { content: 'Said the thing!' } );
+        console.log( '%o requested me to speak in `%o`#%o:\n\t%o',
+          strAuthorTag, spoke.guild.name, spoke.channel.name, mySaying
+        );
+      } ).catch( muted => {
+        switch ( muted.code ) {
+          case 50001 :
+            let noChan = '<#' + speakChannel + '>';
+            interaction.editReply( {
+              content: 'I do not have permission to send messages in ' + noChan
+            } );
+            break;
+          default:
+            console.error( 'Unable to speak: %o', muted );
+            interaction.editReply( { content: 'Couldn\'t say the thing!' } );
+        }
+      } );
     }
     console.error( 'mySaying: %o', mySaying );
     interaction.editReply( { content: 'I don\'t know what to say. :(' } );
