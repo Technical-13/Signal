053ee53cded6fb5ef9cf5c31f150efae27306099
Donald J. Fortier II <Technical_13@yahoo.com>
2024-03-29 12:15:13 -0400

    Update bot.js
    Comments with URLs of some things I'd like to offer.

--- commands/bot.js
+++ commands/bot.js
@@ -3,6 +3,8 @@ module.exports = {
 	description: "Options to find the GitHub repository, report bugs, and suggest features!",
 	cooldown: 3000,
 	async run( interaction, client ) {
-    //STUFF
-  }
+    // https://github.com/sponsors/Technical-13
+    // https://github.com/Technical-13/Signal/issues/new?assignees=&labels=bug&projects=&template=bug_report.md&title=
+    // https://github.com/Technical-13/Signal/issues/new?assignees=&labels=enhancement&projects=&template=feature_request.md&title=
+	}
 }
