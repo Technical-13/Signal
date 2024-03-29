249118d2bef70f4a73829927ca69bb940b7dd1de
Donald J. Fortier II <Technical_13@yahoo.com>
2024-03-29 12:11:34 -0400

    Create bot.js

---
+++ commands/bot.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: "bot",
+	description: "Options to find the GitHub repository, report bugs, and suggest features!",
+	cooldown: 3000,
+	async run( interaction, client ) {
+    //STUFF
+  }
+}
