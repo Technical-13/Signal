93db590f514bde4cd11c21e5a3c679d41e67178f
Donald J. Fortier II <Technical_13@yahoo.com>
2024-03-30 18:10:38 -0400

    Update bot.js
    Direct sponsor links

--- commands/bot.js
+++ commands/bot.js
@@ -8,7 +8,7 @@ module.exports = {
     if ( strTitle ) { strTitle = '&title=' + encodeURI( strTitle ); }
 		switch ( strAction ) {
 			case 'sponsor':
-  			interaction.reply( { content: 'Sponsor me from GitHub:\n:link: <https://github.com/sponsors/Technical-13>', ephemeral: interaction.inGuild() } );
+  			interaction.reply( { content: 'Sponsor me: [Paypal](<https://paypal.me/MagentaRV>) [CashApp](<https://cash.app/$MagentaRV>) [Venmo](<https://venmo.com/@MagentaRV)', ephemeral: interaction.inGuild() } );
   			break;
 			case 'bug': 
   			interaction.reply( { content: 'Report a bug on GitHub:\n:link: <https://github.com/Technical-13/Signal/issues/new?labels=bug&template=bug_report.md' + strTitle + '>', ephemeral: interaction.inGuild() } );
