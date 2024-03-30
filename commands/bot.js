e6ef6b31957d47b4f1bd8af9dd08f433f6b7a683
Donald J. Fortier II <Technical_13@yahoo.com>
2024-03-30 09:12:52 -0400

    Update bot.js

--- commands/bot.js
+++ commands/bot.js
@@ -4,15 +4,17 @@ module.exports = {
 	cooldown: 3000,
 	async run( interaction, client ) {
 		const strAction = ( interaction.options.getString( 'action' ) ?? 'link' );
+    var strTitle = ( interaction.options.getString( 'title' ) ?? '' );
+    if ( strTitle ) { strTitle = '&title=' + strTitle; }
 		switch ( strAction ) {
 			case 'sponsor':
   			interaction.reply( { content: 'Sponsor me from GitHub:\n:link: <https://github.com/sponsors/Technical-13>' } );
   			break;
 			case 'bug': 
-  			interaction.reply( { content: 'Report a bug on GitHub:\n:link: <https://github.com/Technical-13/Signal/issues/new?assignees=&labels=bug&projects=&template=bug_report.md&title=>' } );
+  			interaction.reply( { content: 'Report a bug on GitHub:\n:link: <https://github.com/Technical-13/Signal/issues/new?labels=bug&template=bug_report.md' + strTitle + '>' } );
   			break;
 			case 'feature':
-  			interaction.reply( { content: 'Request a feature on GitHub:\n:link: <https://github.com/Technical-13/Signal/issues/new?assignees=&labels=enhancement&projects=&template=feature_request.md&title=>' } );
+  			interaction.reply( { content: 'Request a feature on GitHub:\n:link: <https://github.com/Technical-13/Signal/issues/new?labels=enhancement&template=feature_request.md' + strTitle + '>' } );
   			break;
 			case 'link' : default :
 		  	interaction.reply( { content: 'You can check me out on GitHub:\n:link: <https://github.com/Technical-13/Signal/tree/master>' } );
