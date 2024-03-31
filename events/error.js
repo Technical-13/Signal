68ec72776b4ea248d3324d08c6916f0b663126c1
Donald J. Fortier II <Technical_13@yahoo.com>
2024-03-31 09:15:40 -0400

    Create error.js

---
+++ events/error.js
@@ -0,0 +1,14 @@
+const objTimeString = require( '../../time.json' );
+var strNow = () => { return ( new Date() ).toLocaleDateString( 'en-us', objTimeString ) };
+
+module.exports = ( client, err ) => {
+  console.error( '%o: ERROR: %o', strNow(), err );
+  
+  if ( err.code === 'ETIMEDOUT' ) {
+    console.error( `${strNow()}\n\tFailed to connect to the Internet on: ${err.split( ' ' )[ ( err.split( ' ' ).length - 1 ) ]}` );
+  } else if ( err.code ===  'ENOTFOUND' ) {
+    console.error( `${strNow()}\n\tFailed to connect to Discord on: ${err.split( ' ' )[ ( err.split( ' ' ).length - 1 ) ]}` );
+  } else {
+    console.error( `${strNow()}:\n\tERROR: ${err}` );
+  }
+};
