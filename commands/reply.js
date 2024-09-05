4e5b10c048c948d31e6ed3ce4ce9957fca496be9
Donald J. Fortier II <Technical_13@yahoo.com>
2024-09-05 08:42:00 -0400

    Update reply.js to include links to the reply and the message replied to in log.

--- commands/reply.js
+++ commands/reply.js
@@ -29,7 +29,7 @@ module.exports = {
         if ( cmdAllowed ) {
           await message.reply( myResponse ).then( async responded => {
             interaction.editReply( { content: 'Responded!' } );
-            logChan.send( 'I replied to <@' + message.author.id + '>\'s message in https://discord.com/channels/' + message.guild.id + '/' + message.channel.id + '/' + message.id + ' at <@' + interaction.user.id + '>\'s request:\n```\n' + myResponse + '\n```' );
+            logChan.send( 'I replied in https://discord.com/channels/' + responded.guild.id + '/' + responded.channel.id + '/' + responded.id + ' to <@' + message.author.id + '>\'s message in https://discord.com/channels/' + message.guild.id + '/' + message.channel.id + '/' + message.id + ' at <@' + interaction.user.id + '>\'s request:\n```\n' + myResponse + '\n```' );
           } );
         } else {
           logErrorChan.send( '<@' + interaction.user.id + '> has no permission to use my `/reply` command from <#' +
