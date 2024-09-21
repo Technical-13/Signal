7490479e07fca44f0196d524788309d9fac21e2e
Donald J. Fortier II <Technical_13@yahoo.com>
2024-09-21 10:07:18 -0400

    Cleanup
    Apparently process.env is not available at this stage or something.

--- functions/server.js
+++ functions/server.js
@@ -1,16 +1,14 @@
-const bot = process.env.BOT_USERNAME;
 const express = require( 'express' );
 const replServer = express();
 const objTimeString = require( '../time.json' );
 var strNow = () => { return ( new Date() ).toLocaleDateString( 'en-us', objTimeString ) };
-
 
 replServer.all( '/', ( req, res ) => { res.send( bot + ' was last restarted: ' + strNow() ); } );
 
 function keepAlive() {
   var server = replServer.listen( 3000, () => {
     const port = server.address().port;
-    console.log( '%s is ready on port: %s', bot, port );
+    console.log( 'Server is ready on port: %s', port );
   } );
 }
 
