eb6ea0c34f21053a818933e7b53ecf0f8964563e
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-05 14:18:38 -0400

    If there's no owner, leave the guild and continue...

--- commands/admin/inspectdb.js
+++ commands/admin/inspectdb.js
@@ -25,9 +25,25 @@ module.exports = {
         const guild = client.guilds.cache.get( guildId );
         const objGuild = guild.toJSON();
         const guildName = objGuild.name;
+        const vanityURLCode = objGuild.vanityURLCode;
+if ( vanityURLCode ) { console.log( '%s has a vanityURLCode: %s', guildName, vanityURLCode ); }//don't know what this looks like in the API...
+        const chanWidget = ( objGuild.widgetEnabled ? objGuild.widgetChannelId : null );
+        const chanRules = objGuild.rulesChannelId;
+        const chanPublicUpdates = objGuild.publicUpdatesChannelId;
+        const chanSafetyAlerts = objGuild.safetyAlertsChannelId;
+        const chanSystem = objGuild.systemChannelId;
+        const chanFirst = guild.channels.cache.filter( chan => { if ( !chan.nsfw && chan.viewable ) { return chan; } } ).first().id;
+        const chanInvite = ( chanWidget || chanRules || chanPublicUpdates || chanSafetyAlerts || chanSystem || chanFirst );
+        const chanLinkUrl = 'https://discordapp.com/channels/' + guildId + '/' + chanInvite;
         const ownerId = objGuild.ownerId;
         const objGuildOwner = guild.members.cache.get( ownerId );
-        const ownerName = ( objGuildOwner ? objGuildOwner.displayName : ( client.users.cache.get( ownerId ) ? client.users.cache.get( ownerId ).displayName : 'unknown' ) );
+        if ( !objGuildOwner ) {
+          await guild.leave()
+            .then( left => { console.log( 'I left guild with no owner!\n%o', left ); } )
+            .catch( stayed => { console.error( 'I could NOT leave guild with no owner!\n%o', stayed ); } );
+          continue;
+        }
+        const ownerName = objGuildOwner.displayName;
         const iconURL = objGuild.iconURL;
         const memberCount = objGuild.memberCount;
         var maximumMembers = objGuild.maximumMembers;
@@ -40,16 +56,6 @@ module.exports = {
         const arrVerificationLevels = [ 'None', 'Low (email)', 'Medium (5m on Discord)', 'High (10m in guild)', 'Very High (phone number)' ];
         const verificationLevel = arrVerificationLevels[ ( objGuild.verificationLevel || 0 ) ];
         const mfaLevel = objGuild.mfaLevel;
-        const vanityURLCode = objGuild.vanityURLCode;
-        if ( vanityURLCode ) { console.log( '%s has a vanityURLCode: %s', guildName, vanityURLCode ); }
-        const chanWidget = ( objGuild.widgetEnabled ? objGuild.widgetChannelId : null );
-        const chanRules = objGuild.rulesChannelId;
-        const chanPublicUpdates = objGuild.publicUpdatesChannelId;
-        const chanSafetyAlerts = objGuild.safetyAlertsChannelId;
-        const chanSystem = objGuild.systemChannelId;
-        const chanFirst = guild.channels.cache.filter( chan => { if ( !chan.nsfw && chan.viewable ) { return chan; } } ).first().id;
-        const chanInvite = ( chanWidget || chanRules || chanPublicUpdates || chanSafetyAlerts || chanSystem || chanFirst );
-        const chanLinkUrl = 'https://discordapp.com/channels/' + guildId + '/' + chanInvite;
         const guildInvite = await guild.invites.create( chanInvite, {
           maxAge: 300, maxUses: 1, reason: 'Single use invite for my botmod, ' + author.displayName + '.  Expires in 5 minutes if not used.'
         } ).then( invite => { return 'https://discord.gg/invite/' + invite.code; } ).catch( errCreateInvite => {
@@ -59,11 +65,9 @@ module.exports = {
                 guildName, objGuild.widgetEnabled, objGuild.widgetChannelId, chanWidget, chanRules, chanPublicUpdates, chanSafetyAlerts, chanSystem, chanFirst );
               break;
             case 50013://Missing permissions
-              if ( objGuildOwner ) {
-                objGuildOwner.send( 'Help!  Please give me `CreateInstantInvite` permission in ' + chanLinkUrl + '!' ).catch( errSendGuildOwner => {
-                  console.error( 'Unable to DM guild owner, %s, for %s to get `CreateInstantInvite` permission:\n%o', ownerName, guildName, errSendGuildOwner );
-                } );
-              } else { console.error( 'Unable to DM unknown guild owner with id:%s, for %s to get `CreateInstantInvite` permission!', ownerId, guildName ); }
+              objGuildOwner.send( 'Help!  Please give me `CreateInstantInvite` permission in ' + chanLinkUrl + '!' ).catch( errSendGuildOwner => {
+                console.error( 'Unable to DM guild owner, %s, for %s to get `CreateInstantInvite` permission:\n%o', ownerName, guildName, errSendGuildOwner );
+              } );
               break;
             default:
               console.error( 'Unable to create an invite for %s:\n%o', guildName, errCreateInvite );
