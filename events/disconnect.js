3766edb220620c26cce186546e23fcc24144e521
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-05 11:48:14 -0400

    Delete events directory

--- events/disconnect.js
+++
@@ -1,6 +0,0 @@
-const objTimeString = require( '../time.json' );
-var strNow = () => { return ( new Date() ).toLocaleDateString( 'en-us', objTimeString ) };
-
-module.exports = ( client, dc ) => {
-  console.log( `${strNow()}:\n\tI\'ve been disconnected with: ${dc}` );
-};
