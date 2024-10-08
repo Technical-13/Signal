df57ba10bc44239c8469e094d26c380ee7182241
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-08 16:45:23 -0400

    Cleanup

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -33,16 +33,12 @@ client.on( 'messageUpdate', async ( oldMessage, newMessage ) => {
   
   if ( hasJunkEmbed ) {
     newMessage.suppressEmbeds( true );
-    const msgCleaned = await newMessage.reply(
-      '<@' + author.id + '>, I cleaned the embeds from your message.\n' +
-      'To avoid this in the future, please wrap links like `<' + arrJunkEmbedURLs[ 0 ] + '>`\n' +
-      'This message will self destruct in 15 seconds.'
-    );
-    for ( let seconds = 14; seconds > 0; seconds-- ) { setTimeout( () => { msgCleaned.edit(
-      '<@' + author.id + '>, I cleaned the embeds from your message.\n' +
-      'To avoid this in the future, please wrap links like `<' + arrJunkEmbedURLs[ 0 ] + '>`\n' +
-      'This message will self destruct in ' + seconds + ' seconds.'
-    ); }, ( 15 - seconds ) * 1000 ); }
-    setTimeout( () => { msgCleaned.delete(); }, 15000 );
+    const baseMsg = '<@' + author.id + '>, I cleaned the embeds from your message.\n' +
+      'To avoid this in the future, please wrap links like `<' + arrJunkEmbedURLs[ 0 ] + '>`\n';
+    const msgCleaned = await newMessage.reply( baseMsg + 'This message will self destruct in 15 seconds.' );
+    for ( let seconds = 14; seconds > 0; seconds-- ) {
+      setTimeout( () => { msgCleaned.edit( baseMsg + 'This message will self destruct in ' + seconds + ' seconds.' ); }, ( 15 - seconds ) * 1000 );
+    }
+    setTimeout( async () => { await msgCleaned.edit( baseMsg ).then( () => { msgCleaned.delete(); } ); }, 15000 );
   }
 } );
