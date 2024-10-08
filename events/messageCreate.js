fab091facd1745f78a112d1cc91e03b99f13a845
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-08 09:59:11 -0400

    Supress useless embed.
    Currently just Geocaching ones - should move the list of bad strings to master bot config DB entry and allow custom per guild ones, eventually.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,19 +5,34 @@ const prefix = client.prefix;
 const cooldown = new Collection();
 const CLIENT_ID = process.env.CLIENT_ID;
 const DEV_GUILD_ID = process.env.DEV_GUILD_ID;
+const OWNER_ID = process.env.OWNER_ID;
 
 client.on( 'messageCreate', async message => {
   const { author, channel, content, guild, mentions } = message;
   if ( author.bot ) return;
   if ( channel.type !== 0 ) return;
   const isDevGuild = ( guild.id == DEV_GUILD_ID );
-  const botOwner = client.users.cache.get( process.env.OWNER_ID );
+  const botOwner = client.users.cache.get( OWNER_ID );
   const isBotOwner = ( author.id === botOwner.id ? true : false );
 // Get botMods, isBotMod
   const objGuildMembers = guild.members.cache;
   const objGuildOwner = objGuildMembers.get( guild.ownerId );
   const isGuildOwner = ( author.id === objGuildOwner.id ? true : false );
   const msgAuthor = await guild.members.cache.get( author.id );
+  
+  const arrJunkEmbedTitles = [
+    'Geocaching: Join the world\'s largest treasure hunt.'
+  ];
+  const arrJunkEmbedURLs = [];
+  const hasJunkEmbed = ( message.embeds.find( embed => {
+    for ( const title of arrJunkEmbedTitles ) {
+      if ( embed.title === title ) {
+        arrJunkEmbedURLs.push( embed.url );
+        return embed;
+      };
+    }
+  } ) ? true : false );
+  
   const hasPrefix = content.startsWith( prefix );
   const meMentionPrefix = '<@' + CLIENT_ID + '>';
   const mePrefix = content.startsWith( meMentionPrefix );
@@ -87,6 +102,12 @@ client.on( 'messageCreate', async message => {
         }
         command.run( client, message, args );
       }
+    }
+  } else {
+    if ( hasJunkEmbed ) {
+      message.suppressEmbeds( true );
+      const msgCleaned = await message.reply( '<@' + author.id + '>, I cleaned the embeds from your message.\nTo avoid this in the future, please wrap links like `<`' + arrJunkEmbedURLs[ 0 ] + '`>`' );
+      msgCleaned.delete( 5000 );
     }
   }
 } );
