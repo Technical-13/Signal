63e210c782a71dde83a0a701f7dfab97a2872f67
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-15 13:39:27 -0400

    Create system.js

---
+++ slashCommands/admin/system.js
@@ -0,0 +1,25 @@
+const botConfig = require( '../../models/BotConfig.js' );
+const { model, Schema } = require( 'mongoose' );
+
+module.exports = {
+  name: 'system',
+  description: 'Change bot configs.',
+  type: ApplicationCommandType.ChatInput,
+  cooldown: 1000,
+  modCmd: true,
+  run: async ( client, interaction ) => {
+    await interaction.deferReply( { ephemeral: true } );
+
+    const bot = client.user;
+    const { channel, guild, options } = interaction;
+    const author = interaction.user;
+    const botOwner = client.users.cache.get( process.env.OWNER_ID );
+    const isBotOwner = ( author.id === botOwner.id ? true : false );
+    const botMods = await botConfig.find();
+    const isBotMod = ( ( botOwner || botMods.indexOf( author.id ) != -1 ) ? true : false );
+
+    if ( !isBotMod ) {
+      return interaction.editReply( { content: 'You are not the boss of me...' } );
+    }
+  }
+};
