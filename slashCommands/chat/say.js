7dc8a839053359a2cd69c2fc2401a95691d52b8c
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-18 18:07:03 -0400

    ReferenceError: InteractionContextType is not defined

--- slashCommands/chat/say.js
+++ slashCommands/chat/say.js
@@ -1,4 +1,4 @@
-const { ApplicationCommandType } = require( 'discord.js' );
+const { ApplicationCommandType, InteractionContextType } = require( 'discord.js' );
 const { model, Schema } = require( 'mongoose' );
 const guildConfigDB = require( '../../models/GuildConfig.js' );
 const userPerms = require( '../../functions/getPerms.js' );
