bd48d03d5c98ab6794914cea4b364e2010857320
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-25 16:19:06 -0400

    Update edit.js

--- slashCommands/chat/edit.js
+++ slashCommands/chat/edit.js
@@ -32,7 +32,7 @@ module.exports = {
     const { isBotMod, hasManageGuild, hasManageMessages, hasMentionEveryone, isWhitelisted, content } = await userPerms( author, guild, true, true );
     if ( content ) { return interaction.editReply( { content: content } ); }
 
-    const canSpeak = ( isBotMod || hasManageGuild || hasManageMessages || isWhitelisted || ? true : false );
+    const canSpeak = ( isBotMod || hasManageGuild || hasManageMessages || isWhitelisted ? true : false );
     const msgID = options.getString( 'message-id' );
     if ( !( /[\d]{18,19}/.test( msgID ) ) ) { return interaction.editReply( { content: '`' + msgID + '` is not a valid `message-id`. Please try again.' } ); }
     const mySaying = options.getString( 'saying' );
