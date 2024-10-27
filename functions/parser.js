d9677033edf0f5030c638dbe7686a38cf78cea86
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-27 07:07:12 -0400

    Update parser.js

--- functions/parser.js
+++ functions/parser.js
@@ -42,16 +42,16 @@ module.exports = async ( rawString, obj = { guild: null, member: null, uptime: n
 
   arrTemplates = rawString.match( /\{\{((?:bot|guild|member)\.[a-z\.]*)\}\}/g );
   var parsed = rawString;
-if ( arrTemplates.length != 0 ) {
-  arrTemplates.forEach( template => {
-    if ( transclusions[ template ] ) { parsed = parsed.replace( template, transclusions[ template ] ); }
-    else if ( notAvailable[ template ] ) { parsed = parsed.replace( template, '*(unknown ' + notAvailable[ template ] + ')*' ); }
-    else {
-      parsed = parsed.replace( template, '[*' + template + '*](<https://github.com/Technical-13/' + bot.username + '/issues/new?labels=enhancement&template=feature_request.md&title=' + encodeURI( 'Please add ' + template + ' to transclude.js' ) + '>)' );
-      console.log( 'Someone tried to transclude %s, search for a GitHub feature request:\n https://github.com/Technical-13/%s/issues?q=%s', chalk.bold.red( template ), bot.username, encodeURI( 'Please add ' + template + ' to transclude.js' ) );
-    }
-  } );
-}
+  if ( arrTemplates.length != 0 ) {
+    arrTemplates.forEach( template => {
+      if ( transclusions[ template ] ) { parsed = parsed.replace( template, transclusions[ template ] ); }
+      else if ( notAvailable[ template ] ) { parsed = parsed.replace( template, '*(unknown ' + notAvailable[ template ] + ')*' ); }
+      else {
+        parsed = parsed.replace( template, '[*' + template + '*](<https://github.com/Technical-13/' + bot.username + '/issues/new?labels=enhancement&template=feature_request.md&title=' + encodeURI( 'Please add ' + template + ' to transclude.js' ) + '>)' );
+        console.log( 'Someone tried to transclude %s, search for a GitHub feature request:\n https://github.com/Technical-13/%s/issues?q=%s', chalk.bold.red( template ), bot.username, encodeURI( 'Please add ' + template + ' to transclude.js' ) );
+      }
+    } );
+  }
 
   return parsed;
 };
