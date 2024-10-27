e4f9b12b184e6456dfbf9e48d1df75bb875f8828
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-27 07:23:17 -0400

    Update parser.js

--- functions/parser.js
+++ functions/parser.js
@@ -9,7 +9,7 @@ module.exports = async ( rawString, obj = { guild: null, member: null, uptime: n
   const uptime = ( obj.uptime ? obj.uptime : null );
   const bot = client.user;
 
-  const currUptime = await duration( client.uptime,  );
+  const currUptime = await duration( client.uptime, uptime );
   const transclusions = {
     '{{bot.name}}': bot.displayName,
     '{{bot.owner.name}}': client.users.cache.get( client.ownerId ).displayName,
@@ -42,7 +42,7 @@ module.exports = async ( rawString, obj = { guild: null, member: null, uptime: n
 
   arrTemplates = rawString.match( /\{\{((?:bot|guild|member)\.[a-z\.]*)\}\}/g );
   var parsed = rawString;
-  if ( arrTemplates.length != 0 ) {
+  if ( arrTemplates ) {
     arrTemplates.forEach( template => {
       if ( transclusions[ template ] ) { parsed = parsed.replace( template, transclusions[ template ] ); }
       else if ( notAvailable[ template ] ) { parsed = parsed.replace( template, '*(unknown ' + notAvailable[ template ] + ')*' ); }
