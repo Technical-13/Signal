89e79cf7aa130829ca147db1a8c93286a4ca0587
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-22 08:27:02 -0500

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -28,8 +28,8 @@ client.on( 'messageCreate', async ( message ) => {
     if ( !applicationId && webhookId === authorId ) return;//It's a webhook
     const allowedBots = [];
     const isAllowedBot = ( allowedBots.indexOf( author.id ) != -1 ? true : false );
-    if ( author.bot && !allowedBots ) return;//It's a bot that is not allowed
-    const { clientId, botOwner, isDevGuild, prefix, isBotOwner, isBotMod, isGlobalWhitelisted, isBlacklisted, isGuildBlacklisted, errors } = await userPerms( author, guild );
+    if ( author.bot && !isAllowedBot ) return;//It's a bot that is not allowed
+    const { botOwner, checkPermission, clientId, errors, isBlacklisted, isBotMod, isBotOwner, isDevGuild, isGlobalWhitelisted, isGuildBlacklisted, prefix } = await userPerms( author, guild );
     if ( errors.hasNoMember ) {
       throw new Error( errors.noMember.console + '\n\tisBot: ' + ( author.bot ? 'true' : 'false' ) + '\n\tapplicationId: ' + applicationId + '\n\twebhookId: ' + webhookId );
     }
