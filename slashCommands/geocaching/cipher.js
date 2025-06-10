85017878eac2c49ef5e83aad490e316d7b455d75
Donald J. Fortier II <Technical_13@yahoo.com>
2025-06-10 14:14:59 -0400

    Typo fixing

--- slashCommands/geocaching/cipher.js
+++ slashCommands/geocaching/cipher.js
@@ -13,10 +13,10 @@ module.exports = {
     { type: 3, name: 'code', description: 'Decode or encode?', required: true,
       choices: [ { name: 'Decode', value: 'decode' }, { name: 'Encode', value: 'encode' } ] },
     { type: 3, name: 'use-type', description: 'Pick a type.',
-      choices: [ { name: 'Letters A-Z', value: 'alphabetic' }, { name: 'Letters A-Z & Numbers 0-9', value: 'alphanumberic' }, { name: 'Numbers 0-9', value: 'numeric' } ] },
+      choices: [ { name: 'Letters A-Z', value: 'alphabetic' }, { name: 'Letters A-Z & Numbers 0-9', value: 'alphanumeric' }, { name: 'Numbers 0-9', value: 'numeric' } ] },
     { type: 10, name: 'numeric', description: 'Characters in the Latin alphabet. (default 5)', minValue: 1, maxValue: 10 },
     { type: 10, name: 'alphabetic', description: 'Characters in the Latin alphabet. (default 13)', minValue: 1, maxValue: 26 },
-    { type: 10, name: 'alphanumberic', description: 'Characters in the Latin alphabet. (default 18)', minValue: 1, maxValue: 36 }
+    { type: 10, name: 'alphanumeric', description: 'Characters in the Latin alphabet. (default 18)', minValue: 1, maxValue: 36 }
   ],
   devOnly: true,
   cooldown: 1000,
