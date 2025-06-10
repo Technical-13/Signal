3e4429ab536a8855fa88881ac8604e6b4c6214e0
Donald J. Fortier II <Technical_13@yahoo.com>
2025-06-10 19:51:17 -0400

    Update getInternationalizations.js
    Add command specific options and common responses.  Making progress.  Next will be to add cmd specific responses THEN I'll work on figuring choices out.

--- functions/getInternationalizations.js
+++ functions/getInternationalizations.js
@@ -23,24 +23,25 @@ module.exports = async ( command ) => {
         i18n.name[ langCode ] = cmdPath.name;
         i18n.description[ langCode ] = cmdPath.description;
         const commonOptions = Object.entries( currLangFile.common.options );
-        commonOptions.forEach( ( opt ) => {/* TRON */console.log( 'opt: %o', opt );/* TROFF */
+        commonOptions.forEach( ( opt ) => {
           i18n.options[ opt[ 0 ] ] = ( i18n.options[ opt[ 0 ] ] ?? { name: {}, description: {} } );
-          const optBuilder = i18n.options[ opt[ 0 ] ];/* TRON */console.log( 'optBuilder: %o', optBuilder );/* TROFF */
+          const optBuilder = i18n.options[ opt[ 0 ] ];
           optBuilder.name[ langCode ] = opt[ 1 ].name;
           optBuilder.description[ langCode ] = opt[ 1 ].description;
           //optBuilder.choices = '';//skip this for now.  Let's get name/desc working first.
         } );
-        /*if ( cmdPath.options ) {
-          const cmdOptions = Object.entries( cmdPath.options );/* TRON /console.log( 'cmdOptions: %o', cmdOptions );/* TROFF /
+        if ( cmdPath.options ) {
+          const cmdOptions = Object.entries( cmdPath.options );
           cmdOptions.forEach( ( opt ) => {
+            i18n.options[ opt[ 0 ] ] = ( i18n.options[ opt[ 0 ] ] ?? { name: {}, description: {} } );
             const optBuilder = i18n.options[ opt[ 0 ] ];
-            optBuilder.name = opt[ 1 ].name;
-            optBuilder.description = opt[ 1 ].description;
+            optBuilder.name[ langCode ] = opt[ 1 ].name;
+            optBuilder.description[ langCode ] = opt[ 1 ].description;
             //optBuilder.choices = '';//skip this for now.  Let's get name/desc working first.
           } );
-        }//*/
-        /*const commonResponses = Object.entries( currLangFile.common.responses );/* TRON /console.log( 'commonResponses: %o', commonResponses );/* TROFF /
-        commonResponses.forEach( ( res ) => { i18n.responses[ res[ 0 ] ] = res[ 1 ]; } );//*/
+        }
+        const commonResponses = Object.entries( currLangFile.common.responses );
+        commonResponses.forEach( ( res ) => { i18n.responses[ res[ 0 ] ] = res[ 1 ]; } );
         /*if ( cmdPath.responses ) {
           const cmdResponses = Object.entries( cmdPath.responses );/* TRON /console.log( 'cmdResponses: %o', cmdResponses );/* TROFF /
           cmdResponses.forEach( ( res ) => { i18n.responses[ res[ 0 ] ] = res[ 1 ]; } );
