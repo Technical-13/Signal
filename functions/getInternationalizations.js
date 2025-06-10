c699d3b53d5426205d37e6c7388c7fff71780df0
Donald J. Fortier II <Technical_13@yahoo.com>
2025-06-10 19:34:57 -0400

    Update getInternationalizations.js
    Let's back up and build this one little bite at a time...

--- functions/getInternationalizations.js
+++ functions/getInternationalizations.js
@@ -21,29 +21,30 @@ module.exports = async ( command ) => {
         const currLangFile = require( '../i18n/' + filename );
         const cmdPath = currLangFile[ command.group ][ command.name ];
         i18n.name[ langCode ] = cmdPath.name;
-        i18n.description[ langCode ] = cmdPath.description;
-        const commonOptions = Object.entries( currLangFile.common.options );
+        i18n.description[ langCode ] = cmdPath.description;/* TRON */console.log( 'i18n: %o', i18n );/* TROFF */
+        /*const commonOptions = Object.entries( currLangFile.common.options );
         commonOptions.forEach( ( opt ) => {
+          i18n.options[ opt[ 0 ] ] = {};
           const optBuilder = i18n.options[ opt[ 0 ] ];
           optBuilder.name = opt[ 1 ].name;
           optBuilder.description = opt[ 1 ].description;
           //optBuilder.choices = '';//skip this for now.  Let's get name/desc working first.
-        } );
-        if ( cmdPath.options ) {
-          const cmdOptions = Object.entries( cmdPath.options );/* TRON */console.log( 'cmdOptions: %o', cmdOptions );/* TROFF */
+        } );//*/
+        /*if ( cmdPath.options ) {
+          const cmdOptions = Object.entries( cmdPath.options );/* TRON /console.log( 'cmdOptions: %o', cmdOptions );/* TROFF /
           cmdOptions.forEach( ( opt ) => {
             const optBuilder = i18n.options[ opt[ 0 ] ];
             optBuilder.name = opt[ 1 ].name;
             optBuilder.description = opt[ 1 ].description;
             //optBuilder.choices = '';//skip this for now.  Let's get name/desc working first.
           } );
-        }
-        const commonResponses = Object.entries( currLangFile.common.responses );/* TRON */console.log( 'commonResponses: %o', commonResponses );/* TROFF */
-        commonResponses.forEach( ( res ) => { i18n.responses[ res[ 0 ] ] = res[ 1 ]; } );
-        if ( cmdPath.responses ) {
-          const cmdResponses = Object.entries( cmdPath.responses );/* TRON */console.log( 'cmdResponses: %o', cmdResponses );/* TROFF */
+        }//*/
+        /*const commonResponses = Object.entries( currLangFile.common.responses );/* TRON /console.log( 'commonResponses: %o', commonResponses );/* TROFF /
+        commonResponses.forEach( ( res ) => { i18n.responses[ res[ 0 ] ] = res[ 1 ]; } );//*/
+        /*if ( cmdPath.responses ) {
+          const cmdResponses = Object.entries( cmdPath.responses );/* TRON /console.log( 'cmdResponses: %o', cmdResponses );/* TROFF /
           cmdResponses.forEach( ( res ) => { i18n.responses[ res[ 0 ] ] = res[ 1 ]; } );
-        }
+        }//*/
       }
       else {
         console.warn( '%s is a language not currently supported by Discord.', i18n.locales[ langCode ] );
