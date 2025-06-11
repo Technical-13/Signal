4fc1f30953341ced08364e00b7c9a9a5e1a8617e
Donald J. Fortier II <Technical_13@yahoo.com>
2025-06-10 21:50:40 -0400

    Update getInternationalizations.js
    fix langCode

--- functions/getInternationalizations.js
+++ functions/getInternationalizations.js
@@ -20,10 +20,10 @@ module.exports = async ( command, getLocales = false ) => {
     if ( getLocales ) { i18n.locales = locales }
     const files = fs.readdirSync( './i18n/' ).filter( file => file.endsWith( '.json' ) );
     const langs = files.map( file => file.replace( '.json', '' ) );
-    langs.forEach( ( lang ) => {
-      const languageNames = new Intl.DisplayNames( [ lang ], { type: 'language' } );
+    langs.forEach( ( langCode ) => {
+      const languageNames = new Intl.DisplayNames( [ langCode ], { type: 'language' } );
       if ( langCodes.indexOf( langCode ) !== -1 ) {
-        const currLangFile = require( '../i18n/' + lang + '.json' );
+        const currLangFile = require( '../i18n/' + langCode + '.json' );
         const cmdPath = currLangFile[ command.group ][ command.name ];
         i18n.name[ langCode ] = cmdPath.name;
         i18n.description[ langCode ] = cmdPath.description;
