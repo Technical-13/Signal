fff4c1e5c45864fb2bfb2ba17b9552d71708186e
Donald J. Fortier II <Technical_13@yahoo.com>
2025-06-11 08:44:02 -0400

    Update getInternationalizations.js
    Needs langCode...

--- functions/getInternationalizations.js
+++ functions/getInternationalizations.js
@@ -4,7 +4,7 @@ const { Locale } = require( 'discord-api-types/v10' );
 const chalk = require( 'chalk' );
 const strScript = chalk.hex( '#FFA500' ).bold( './functions/getInternationalizations.js' );
 const enNames = new Intl.DisplayNames( [ 'en' ], { type: 'language' } );
-const getOptions = ( options, objOpt = {} ) => {
+const getOptions = ( langCode, options, objOpt = {} ) => {
   options.forEach( async ( opt ) => {
     objOpt[ opt[ 0 ] ] = ( objOpt[ opt[ 0 ] ] ?? { name: {}, description: {} } );
     const optBuilder = objOpt[ opt[ 0 ] ];
@@ -13,7 +13,7 @@ const getOptions = ( options, objOpt = {} ) => {
     optBuilder.description[ langCode ] = data.description;
     if ( data.options ) {
       if ( !optBuilder.options ) { optBuilder.options = {}; }
-      optBuilder.options = await getOptions( data.options );
+      optBuilder.options = await getOptions( langCode, data.options );
     }
     if ( data.choices ) {
       if ( !optBuilder.choices ) { optBuilder.choices = []; }
@@ -53,7 +53,7 @@ module.exports = async ( command, getLocales = false ) => {
         i18n.name[ langCode ] = cmdPath.name;
         i18n.description[ langCode ] = cmdPath.description;
         const commonOptions = Object.entries( currLangFile.common.options );
-        /* TRON */console.log( 'Test recursive function:\n\tcommonOptions: %o', await getOptions( commonOptions ) );/* TROFF */
+        /* TRON */console.log( 'Test recursive function:\n\tcommonOptions: %o', await getOptions( langCode, commonOptions ) );/* TROFF */
         commonOptions.forEach( ( opt ) => {
           i18n.options[ opt[ 0 ] ] = ( i18n.options[ opt[ 0 ] ] ?? { name: {}, description: {} } );
           const optBuilder = i18n.options[ opt[ 0 ] ];
