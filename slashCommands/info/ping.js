10a16b9475b965a5b58e4b936cfa02dbec0866ca
Donald J. Fortier II <Technical_13@yahoo.com>
2025-06-12 13:56:31 -0400

    Update ping.js
    Can I make a function to pre-parse all the strings?  Maybe I can just return parsed strings from getI18n.js?  Not sure best practice for this...  Need to think more about it...

--- slashCommands/info/ping.js
+++ slashCommands/info/ping.js
@@ -4,6 +4,20 @@ const parse = require( '../../functions/parser.js' );
 const getI18n = require( '../../functions/getInternationalizations.js' );
 const cmdData = { group: 'info', name: 'ping' };
 const strScript = chalk.hex( '#FFA500' ).bold( './slashCommands/' + cmdData.group + '/' + cmdData.name + '.js' );
+const getResponses = async ( interaction ) => {
+  const { guild, locale, user } = interaction;
+  const { langs, responses } = await getI18n( command );
+  const guildLang = ( langs.indexOf( guild.preferredLocale ) === -1 ?  'en-US' : guild.preferredLocale );
+  const useLang = ( langs.indexOf( locale ) === -1 ? guildLang : locale );
+  const results = {}; results[ useLang ] = {}; results[ guildLang ] = {};
+  const arrResponses = Object.entries( responses );
+  arrResponses.forEach( resp => {
+    results[ useLang ][ resp[ 0 ] ] = await parse( resp[ 1 ][ useLang ], { author: user, guild: guild } );
+    results[ guildLang ][ resp[ 0 ] ] = await parse( resp[ 1 ][ guildLang ], { author: user, guild: guild } );
+    results[ 'en-US' ][ resp[ 0 ] ] = await parse( resp[ 1 ][ 'en-US' ], { author: user, guild: guild } );
+  } );
+  return results;
+}
 
 module.exports = {
   name: cmdData.name,
@@ -16,11 +30,11 @@ module.exports = {
   run: async ( client, interaction ) => {
     const command = client.slashCommands.get( cmdData.name );
     try {
-      const { langs, responses } = await getI18n( command );
       const { guild, locale } = interaction;
       const guildLang = ( langs.indexOf( guild.preferredLocale ) === -1 ?  'en-US' : guild.preferredLocale );
       const useLang = ( langs.indexOf( locale ) === -1 ? guildLang : locale );
-      interaction.reply( { content: await parse( responses.pong[ useLang ] ), ephemeral: interaction.inGuild() } );
+      const useResponses = getResponses( interaction );
+      interaction.reply( { content: await parse( useResponses[ useLang ].pong ), ephemeral: interaction.inGuild() } );
     }
     catch ( errObject ) { console.error( 'Uncaught error in %s:\n\t%s', strScript, errObject.stack ); }
   }
