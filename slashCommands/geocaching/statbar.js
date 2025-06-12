9a2efba8ea2106cc55af3db16712927f2e3cf9a4
Donald J. Fortier II <Technical_13@yahoo.com>
2025-06-12 08:48:54 -0400

    Update statbar.js
    Fixed and add tags for later when I move all the localization into the slashComand event handler so I can just replace `/*§(.*)§*/` with {empty space}.

--- slashCommands/geocaching/statbar.js
+++ slashCommands/geocaching/statbar.js
@@ -17,89 +17,89 @@ module.exports = {
     pl: 'Pokaż Project-GC StatBar dla użytkownika.'
   },
   options: [ {
-    name: 'gc-name',
+    name: 'gc-name'/*§<!--START-->*/,
     name_localizations: {
       de: 'gc-name',
       fr: 'nom-gc',
       fi: 'gc-nimi',
       pl: 'gc-name',
-      'sv-SE': 'gc-namn' },
-    description: 'The case-sensitive Geocaching.com username.',
+      'sv-SE': 'gc-namn' }/*<!--END-->§*/,
+    description: 'The case-sensitive Geocaching.com username.'/*§<!--START-->*/,
     description_localizations: {
       de: 'Der Geocaching.com-Benutzername, bei dem die Groß-/Kleinschreibung beachtet werden muss.',
       fi: 'Geocaching.com-käyttäjänimi, kirjainkoko merkitsevä.',
-      pl: 'W nazwie użytkownika Geocaching.com rozróżniana jest wielkość liter.' },
+      pl: 'W nazwie użytkownika Geocaching.com rozróżniana jest wielkość liter.' }/*<!--END-->§*/,
     type: 3
   }, {
-    name: 'discord-user',
+    name: 'discord-user'/*§<!--START-->*/,
     name_localizations: {
       de: 'discord-benutzer',
       fr: 'utilisateur-discord',
       fi: 'discord-käyttäjä',
       pl: 'discord-użytkownik',
-      'sv-SE': 'discord-användare' },
-    description: 'Discord member (requires nickname to be set if different from GC name).',
-    description_localizations: {
-      de: 'Discord-Mitglied (erfordert das Festlegen eines Spitznamens, wenn dieser vom GC-Namen abweicht).',
-      fi: 'Discord-jäsen (vaatii nimimerkin asettamisen, jos se on eri kuin GC-nimi).',
-      pl: 'Członek Discord (wymaga ustawienia pseudonimu, jeśli różni się od nazwy GC).' },
-    type: 6
+      'sv-SE': 'discord-användare' }/*<!--END-->§*/,
+    description: 'Discord member (requires nickname to be set if different from GC name).'/*§<!--START-->*/,
+  description_localizations: {
+    de: 'Discord-Mitglied (erfordert das Festlegen eines Spitznamens, wenn dieser vom GC-Namen abweicht).',
+    fi: 'Discord-jäsen (vaatii nimimerkin asettamisen, jos se on eri kuin GC-nimi).',
+    pl: 'Członek Discord (wymaga ustawienia pseudonimu, jeśli różni się od nazwy GC).' }/*<!--END-->§*/,
+  type: 6
   }, {
-    name: 'labcaches',
-    description: 'Should I include labcaches? (default: true)',
-    type : 5
+  name: 'labcaches',
+  description: 'Should I include labcaches? (default: true)',
+  type : 5
   } ],
   type: ApplicationCommandType.ChatInput,
   contexts: [ InteractionContextType.Guild ],
   cooldown: 3000,
   run: async ( client, interaction ) => {
-    const command = client.slashCommands.get( 'statbar' );
-    const { langs, responses } = await getI18n( command );
-    try {
-      await interaction.deferReply( { ephemeral: true } );
-      const { channel, guild, locale, options, user: author } = interaction;
-      const guildLang = ( langs.indexOf( guild.preferredLocale ) === -1 ?  'en-US' : guild.preferredLocale );
-      const useLang = ( langs.indexOf( locale ) === -1 ? guildLang : locale );
-      const members = guild.members.cache;
-      const { content } = await userPerms( author, guild );
-      if ( content ) { return interaction.editReply( { content: content } ); }
+  const command = client.slashCommands.get( 'statbar' );
+  const { langs, responses } = await getI18n( command );
+  try {
+    await interaction.deferReply( { ephemeral: true } );
+    const { channel, guild, locale, options, user: author } = interaction;
+    const guildLang = ( langs.indexOf( guild.preferredLocale ) === -1 ?  'en-US' : guild.preferredLocale );
+    const useLang = ( langs.indexOf( locale ) === -1 ? guildLang : locale );
+    const members = guild.members.cache;
+    const { content } = await userPerms( author, guild );
+    if ( content ) { return interaction.editReply( { content: content } ); }
 
-      const today = ( new Date() );
-      const intYear = today.getFullYear();
-      const intMonthNow = today.getMonth();
-      const intMonth = ( intMonthNow < 9 ? '0' : '' ) + ( intMonthNow + 1 ).toString();
-      const intDayNow = today.getDate();
-      const intDay = ( intDayNow <= 9 ? '0' : '' ) + intDayNow.toString();
+    const today = ( new Date() );
+    const intYear = today.getFullYear();
+    const intMonthNow = today.getMonth();
+    const intMonth = ( intMonthNow < 9 ? '0' : '' ) + ( intMonthNow + 1 ).toString();
+    const intDayNow = today.getDate();
+    const intDay = ( intDayNow <= 9 ? '0' : '' ) + intDayNow.toString();
 
-      const strAuthorDisplayName = members.get( author.id ).displayName;
-      const strInputString = ( options.getString( 'gc-name' ) || null );
-      const objInputString = ( members.find( mbr => mbr.displayName === strInputString ) || null );
-      const objInputUser = ( options.getUser( 'discord-user' ) || null );
-      const strInputUserDisplayName = ( objInputUser ? members.get( objInputUser.id ).displayName : strInputString );
-      const isAuthor = ( ( !strInputString && !objInputUser ) || author.id === objInputString?.id || strInputUserDisplayName === strAuthorDisplayName ? true : false );
-      const strUseName = ( strInputUserDisplayName ? strInputUserDisplayName : strAuthorDisplayName );
-      const encName = encodeURI( strUseName ).replace( '&', '%26' );
-      const strLabcaches = ( options.getBoolean( 'labcaches' ) ? '&includeLabcaches' : '' );
-      const { doLogs, chanDefault, chanError, strClosing } = await getGuildConfig( guild );
+    const strAuthorDisplayName = members.get( author.id ).displayName;
+    const strInputString = ( options.getString( 'gc-name' ) || null );
+    const objInputString = ( members.find( mbr => mbr.displayName === strInputString ) || null );
+    const objInputUser = ( options.getUser( 'discord-user' ) || null );
+    const strInputUserDisplayName = ( objInputUser ? members.get( objInputUser.id ).displayName : strInputString );
+    const isAuthor = ( ( !strInputString && !objInputUser ) || author.id === objInputString?.id || strInputUserDisplayName === strAuthorDisplayName ? true : false );
+    const strUseName = ( strInputUserDisplayName ? strInputUserDisplayName : strAuthorDisplayName );
+    const encName = encodeURI( strUseName ).replace( '&', '%26' );
+    const strLabcaches = ( options.getBoolean( 'labcaches' ) ? '&includeLabcaches' : '' );
+    const { doLogs, chanDefault, chanError, strClosing } = await getGuildConfig( guild );
 
-      channel.send( { content:
-        responses.statbarFor[ useLang ] + ' ' +
-        ( !objInputUser ? ( !objInputString ? ( !isAuthor ? '`' + strUseName + '`' : '<@' + author.id + '>' ) : '<@' + objInputString.id + '>' ) : '<@' + objInputUser.id + '>' ) +
-        ( isAuthor ? '' : ' ' + await parse( responses.requestedBy[ useLang ], { author: author } ) ) +
-        '\nhttps://cdn2.project-gc.com/statbar.php?quote=https://discord.me/Geocaching%20-%20' + intYear + '-' + intMonth + '-' + intDay + strLabcaches + '&user=' + encName
-      } )
-      .then( async sentMsg => {
-        if ( doLogs && !isAuthor ) {
-          chanDefault.send( { content:
-            responses.sharedFor[ guildLang ] + ( !objInputUser ? ( !objInputString ? '`' + strUseName + '`' : '<@' + objInputString.id + '>' ) : '<@' + objInputUser.id + '>' ) +
-            responses.in[ guildLang ] + ' <#' + channel.id + '> ' + await parse( responses.requestedBy[ guildLang ], { author: author } ) + ' ' + strClosing } )
-          .then( sentLog => { interaction.deleteReply(); } )
-          .catch( async errLog => { await errHandler( errLog, { chanType: 'default', command: 'statbar', channel: channel, type: 'logLogs' } ); } );
-        }
-        else { interaction.deleteReply(); }
-      } )
-      .catch( async errSend => { interaction.editReply( await errHandler( errSend, { command: 'statbar', channel: channel, type: 'errSend' } ) ); } );
+    channel.send( { content:
+    responses.statbarFor[ useLang ] + ' ' +
+    ( !objInputUser ? ( !objInputString ? ( !isAuthor ? '`' + strUseName + '`' : '<@' + author.id + '>' ) : '<@' + objInputString.id + '>' ) : '<@' + objInputUser.id + '>' ) +
+    ( isAuthor ? '' : ' ' + await parse( responses.requestedBy[ useLang ], { author: author } ) ) +
+    '\nhttps://cdn2.project-gc.com/statbar.php?quote=https://discord.me/Geocaching%20-%20' + intYear + '-' + intMonth + '-' + intDay + strLabcaches + '&user=' + encName
+    } )
+    .then( async sentMsg => {
+    if ( doLogs && !isAuthor ) {
+      chanDefault.send( { content:
+      responses.sharedFor[ guildLang ] + ( !objInputUser ? ( !objInputString ? '`' + strUseName + '`' : '<@' + objInputString.id + '>' ) : '<@' + objInputUser.id + '>' ) +
+      responses.in[ guildLang ] + ' <#' + channel.id + '> ' + await parse( responses.requestedBy[ guildLang ], { author: author } ) + ' ' + strClosing } )
+      .then( sentLog => { interaction.deleteReply(); } )
+      .catch( async errLog => { await errHandler( errLog, { chanType: 'default', command: 'statbar', channel: channel, type: 'logLogs' } ); } );
     }
-    catch ( errObject ) { console.error( 'Uncaught error in %s:\n\t%s', strScript, errObject.stack ); }
+    else { interaction.deleteReply(); }
+    } )
+    .catch( async errSend => { interaction.editReply( await errHandler( errSend, { command: 'statbar', channel: channel, type: 'errSend' } ) ); } );
+  }
+  catch ( errObject ) { console.error( 'Uncaught error in %s:\n\t%s', strScript, errObject.stack ); }
   }
 };
