a15c0719ec046262e31e7d829b6f45fc2c97e2d0
Donald J. Fortier II <Technical_13@yahoo.com>
2025-06-13 13:25:45 -0400

    Update parser.js
    Add `{{channel.*}}`, data from `interaction`, and i18n responses.

--- functions/parser.js
+++ functions/parser.js
@@ -4,43 +4,50 @@ const discord = require( 'discord.js' );
 const objTimeString = require( '../jsonObjects/time.json' );
 const chalk = require( 'chalk' );
 const duration = require( './duration.js' );
-const strScript = chalk.hex( '#FFA500' ).bold( './functions/parser.js' );
+const fData = { name: 'parser', type: 'functions' };
+const strScript = chalk.hex( '#FFA500' ).bold( './' + fData.type + '/' + fData.name + '.js' );
 
-module.exports = async ( rawString, obj = { author: null, guild: null, member: null, uptime: null } ) => {
+module.exports = async ( rawString, obj = { author: null, guild: null, interaction: null, member: null, uptime: null, useLang: null } ) => {
   try {
-    const author = ( obj.author ? obj.author : null );
-    const member = ( obj.member ? obj.member : null );
-    const guild = ( obj.guild ? obj.guild : ( author ? author.guild : ( member ? member.guild : null ) ) );
+    const interaction = ( obj.interaction ?? null );
+    const { channel, iGuild: guild, locale, options, user } = ( interaction ?? { channel: null, guild: null, locale: null, options: null, user: null } );
+    const author = ( obj.author ? obj.author : ( user ?? null ) );
+    const member = ( obj.member ?? null );
+    const guild = ( obj.guild ? obj.guild : ( iGuild ?? ( author ? author.guild : ( member ? member.guild : null ) ) ) );
+    const useLang = ( obj.useLang ?? options?.getString( 'language' ) ?? locale ?? guild?.preferedLocale ?? 'en-US' );
     const uptime = ( obj.uptime ? obj.uptime : null );
+    // /* Something just came up that is time sensative, adding these to the parser is going to have to wait. */
+    // const targetChannel = ( options?.getChannel( 'channel' ) ?? null );
+    // const targetGuild = ( options?.getString( 'guild' ) ?? null );
+    // const targetUser = ( options?.getUser( 'discord-user' ) ?? null );
     const ageUnits = { getDecades: true, getYears: true, getMonths: true, getWeeks: true, getDays: true, getHours: false, getMinutes: false };
     const bot = client.user;
 
     const transclusions = {
       '{{bot.age}}': await duration( Date.now() - bot.createdTimestamp, ageUnits ),
-      '{{bot.guilds}}': client.guilds.cache.size.toLocaleString(),
-      '{{bot.latency}}': Math.round( client.ws.ping ).toString(),
-      '{{bot.members}}': client.users.cache.size.toLocaleString(),
+      '{{bot.guilds}}': client.guilds.cache.size.toLocaleString( useLang ),
+      '{{bot.latency}}': Math.round( client.ws.ping ).toLocaleString( useLang ),
+      '{{bot.members}}': client.users.cache.size.toLocaleString( useLang ),
       '{{bot.name}}': bot.displayName,
       '{{bot.owner.name}}': client.users.cache.get( client.ownerId ).displayName,
       '{{bot.owner.ping}}': '<@' + client.ownerId + '>',
       '{{bot.ping}}': '<@' + client.id + '>',
-      '{{bot.since}}': bot.createdAt.toLocaleTimeString( 'en-US', objTimeString ),
-      '{{bot.servers}}': client.guilds.cache.size.toLocaleString(),
-      '{{bot.users}}': client.users.cache.size.toLocaleString(),
+      '{{bot.since}}': bot.createdAt.toLocaleTimeString( useLang, ( useLang === 'en-US' ? objTimeString : null ) ),
+      '{{bot.servers}}': client.guilds.cache.size.toLocaleString( useLang ),
+      '{{bot.users}}': client.users.cache.size.toLocaleString( useLang ),
       '{{bot.uptime}}': await duration( client.uptime, uptime ),
       '{{bot.version.djs}}': 'v' + discord.version,
       '{{bot.version.node}}': process.version
     };
     const notAvailable = {};
-
     if ( author ) {
       transclusions[ '{{author.age}}' ] = await duration( Date.now() - author.createdTimestamp, ageUnits );
       transclusions[ '{{author.guild.age}}' ] = await duration( Date.now() - author.joinedTimestamp, ageUnits );
       transclusions[ '{{author.name}}' ] = author.displayName;
       transclusions[ '{{author.ping}}' ] = '<@' + author.id + '>';
       transclusions[ '{{author.server.age}}' ] = await duration( Date.now() - author.joinedTimestamp, ageUnits );
-      transclusions[ '{{author.since}}' ] = author.createdAt.toLocaleTimeString( 'en-US', objTimeString );
-      transclusions[ '{{author.user.since}}' ] = author.createdAt.toLocaleTimeString( 'en-US', objTimeString );
+      transclusions[ '{{author.since}}' ] = author.createdAt.toLocaleTimeString( useLang, ( useLang === 'en-US' ? objTimeString : null ) );
+      transclusions[ '{{author.user.since}}' ] = author.createdAt.toLocaleTimeString( useLang, ( useLang === 'en-US' ? objTimeString : null ) );
     }
     else {
       notAvailable[ '{{author.age}}' ] = 'author';
@@ -51,33 +58,45 @@ module.exports = async ( rawString, obj = { author: null, guild: null, member: n
       notAvailable[ '{{author.since}}' ] = 'author';
       notAvailable[ '{{author.user.since}}' ] = 'author';
     }
-
     if ( author && guild ) {
-      transclusions[ '{{author.member.since}}' ] = guild.members.cache.get( author.id ).joinedAt.toLocaleTimeString( 'en-US', objTimeString );
+      transclusions[ '{{author.member.since}}' ] = guild.members.cache.get( author.id ).joinedAt.toLocaleTimeString( useLang, ( useLang === 'en-US' ? objTimeString : null ) );
     }
     else {
       notAvailable[ '{{author.member.since}}' ] = 'author';
     }
-
+    if ( channel ) {
+      transclusions[ '{{channel.age}}' ] = await duration( Date.now() - channel.createdTimestamp, ageUnits );
+      transclusions[ '{{channel.members}}' ] = channel.members.cache.size.toLocaleString( useLang );
+      transclusions[ '{{channel.name}}' ] = channel.name;
+      transclusions[ '{{channel.since}}' ] = channel.createdAt.toLocaleTimeString( useLang, ( useLang === 'en-US' ? objTimeString : null ) );
+      transclusions[ '{{channel.topic}}' ] = channel.topic;
+    }
+    else {
+      notAvailable[ '{{channel.age}}' ] = 'channel';
+      notAvailable[ '{{channel.members}}' ] = 'channel';
+      notAvailable[ '{{channel.name}}' ] = 'channel';
+      notAvailable[ '{{channel.since}}' ] = 'channel';
+      notAvailable[ '{{channel.topic}}' ] = 'channel';
+    }
     if ( guild ) {
       transclusions[ '{{bot.guild.age}}' ] = await duration( Date.now() - guild.members.cache.get( bot.id ).joinedTimestamp, ageUnits );
-      transclusions[ '{{bot.guild.since}}' ] = guild.members.cache.get( bot.id ).joinedAt.toLocaleTimeString( 'en-US', objTimeString );
+      transclusions[ '{{bot.guild.since}}' ] = guild.members.cache.get( bot.id ).joinedAt.toLocaleTimeString( useLang, ( useLang === 'en-US' ? objTimeString : null ) );
       transclusions[ '{{bot.member.age}}' ] = await duration( Date.now() - guild.members.cache.get( bot.id ).joinedTimestamp, ageUnits );
-      transclusions[ '{{bot.member.since}}' ] = guild.members.cache.get( bot.id ).joinedAt.toLocaleTimeString( 'en-US', objTimeString );
+      transclusions[ '{{bot.member.since}}' ] = guild.members.cache.get( bot.id ).joinedAt.toLocaleTimeString( useLang, ( useLang === 'en-US' ? objTimeString : null ) );
       transclusions[ '{{bot.server.age}}' ] = await duration( Date.now() - guild.members.cache.get( bot.id ).joinedTimestamp, ageUnits );
-      transclusions[ '{{bot.server.since}}' ] = guild.members.cache.get( bot.id ).joinedAt.toLocaleTimeString( 'en-US', objTimeString );
+      transclusions[ '{{bot.server.since}}' ] = guild.members.cache.get( bot.id ).joinedAt.toLocaleTimeString( useLang, ( useLang === 'en-US' ? objTimeString : null ) );
       transclusions[ '{{guild.age}}' ] = await duration( Date.now() - guild.createdTimestamp, ageUnits );
       transclusions[ '{{guild.owner.name}}' ] = guild.members.cache.get( guild.ownerId ).displayName;
       transclusions[ '{{guild.owner.ping}}' ] = '<@' + guild.ownerId + '>';
-      transclusions[ '{{guild.members}}' ] = guild.members.cache.size.toLocaleString();
+      transclusions[ '{{guild.members}}' ] = guild.members.cache.size.toLocaleString( useLang );
       transclusions[ '{{guild.name}}' ] = guild.name;
-      transclusions[ '{{guild.since}}' ] = guild.createdAt.toLocaleTimeString( 'en-US', objTimeString );
+      transclusions[ '{{guild.since}}' ] = guild.createdAt.toLocaleTimeString( useLang, ( useLang === 'en-US' ? objTimeString : null ) );
       transclusions[ '{{server.age}}' ] = await duration( Date.now() - guild.createdTimestamp, ageUnits );
       transclusions[ '{{server.owner.name}}' ] = guild.members.cache.get( guild.ownerId ).displayName;
       transclusions[ '{{server.owner.ping}}' ] = '<@' + guild.ownerId + '>';
-      transclusions[ '{{server.members}}' ] = guild.members.cache.size.toLocaleString();
+      transclusions[ '{{server.members}}' ] = guild.members.cache.size.toLocaleString( useLang );
       transclusions[ '{{server.name}}' ] = guild.name;
-      transclusions[ '{{server.since}}' ] = guild.createdAt.toLocaleTimeString( 'en-US', objTimeString );
+      transclusions[ '{{server.since}}' ] = guild.createdAt.toLocaleTimeString( useLang, ( useLang === 'en-US' ? objTimeString : null ) );
     }
     else {
       notAvailable[ '{{bot.guild.age}}' ] = 'guild';
@@ -99,15 +118,14 @@ module.exports = async ( rawString, obj = { author: null, guild: null, member: n
       notAvailable[ '{{server.name}}' ] = 'guild';
       notAvailable[ '{{server.since}}' ] = 'guild';
     }
-
     if ( member ) {
       transclusions[ '{{member.age}}' ] = await duration( Date.now() - member.user.createdTimestamp, ageUnits );
       transclusions[ '{{member.guild.age}}' ] = await duration( Date.now() - member.joinedTimestamp, ageUnits );
       transclusions[ '{{member.name}}' ] = member.displayName;
       transclusions[ '{{member.ping}}' ] = '<@' + member.id + '>';
       transclusions[ '{{member.server.age}}' ] = await duration( Date.now() - member.joinedTimestamp, ageUnits );
-      transclusions[ '{{member.since}}' ] = guild.members.cache.get( member.id ).joinedAt.toLocaleTimeString( 'en-US', objTimeString );
-      transclusions[ '{{member.user.since}}' ] = member.user.createdAt.toLocaleTimeString( 'en-US', objTimeString );
+      transclusions[ '{{member.since}}' ] = guild.members.cache.get( member.id ).joinedAt.toLocaleTimeString( useLang, ( useLang === 'en-US' ? objTimeString : null ) );
+      transclusions[ '{{member.user.since}}' ] = member.user.createdAt.toLocaleTimeString( useLang, ( useLang === 'en-US' ? objTimeString : null ) );
     }
     else {
       notAvailable[ '{{member.age}}' ] = 'member';
@@ -119,15 +137,15 @@ module.exports = async ( rawString, obj = { author: null, guild: null, member: n
       notAvailable[ '{{member.user.since}}' ] = 'member';
     }
 
-    arrTemplates = rawString.match( /\{\{((?:author|bot|guild|member|server)\.[a-z\.]*)\}\}/g );
+    arrTemplates = rawString.match( /\{\{((?:author|bot|channel|guild|member|server)\.[a-z\.]*)\}\}/g );
     var parsed = rawString;
     if ( arrTemplates ) {
       arrTemplates.forEach( template => {
         if ( transclusions[ template ] ) { parsed = parsed.replace( template, transclusions[ template ] ); }
         else if ( notAvailable[ template ] ) { parsed = parsed.replace( template, '*(unknown ' + notAvailable[ template ] + ')*' ); }
         else {
-          parsed = parsed.replace( template, '[*' + template + '*](<' + config.issueRepo + '/issues/new?labels=enhancement&template=feature_request.md&title=' + encodeURI( 'Please add ' + template + ' to parser.js' ) + '>)' );
-          console.log( 'Someone tried to transclude %s, search for a GitHub feature request:\n %s/issues?q=%s', chalk.bold.red( template ), config.issueRepo, encodeURI( 'Please add ' + template + ' to parser.js' ) );
+          parsed = parsed.replace( template, '[*' + template + '*](<' + config.issueRepo + '/issues/new?labels=enhancement&template=feature_request.md&title=' + encodeURIComponent( 'Please add ' + template + ' to ' + strScript ) + '>)' );
+          console.log( 'Someone tried to transclude %s, search for a GitHub feature request:\n %s/issues?q=%s', chalk.bold.red( template ), config.issueRepo, encodeURIComponent( 'Please add ' + template + ' to ' + strScript ) );
         }
       } );
     }
