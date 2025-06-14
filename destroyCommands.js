d5cd02c4ced9e2220ab79ed502210db24d1c1dc5
Donald J. Fortier II <Technical_13@yahoo.com>
2025-06-14 13:05:21 -0400

    Update destroyCommands.js

--- destroyCommands.js
+++ destroyCommands.js
@@ -13,21 +13,22 @@ const DEV_GUILD_ID = process.env.DEV_GUILD_ID;
 const commands = [].map( command => command.toJSON() );
 
 ( async () => {
-    try {
-        const rest = await new REST( { version: '10' } ).setToken( TOKEN );
+  try {
+    const rest = await new REST( { version: '10' } ).setToken( TOKEN );
 
-        if ( !DEV_MODE ) {
-            console.log( chalk.yellow( 'Attempting to clear global slash commands...' ) );
-            await rest.put( Routes.applicationCommands( CLIENT_ID ), { body: commands } )
-                .then( () => { console.log( chalk.green( 'Successfully cleared global slash commands.' ) ); } )
-                .catch( errPutRest => { console.error( chalk.red( `Failed to clear global slash commands:\n${errPutRest.stack}` ) ); } );
-        } else {
-            console.log( chalk.yellow( 'Attempting to clear developer guild slash commands...' ) );
-            await rest.put( Routes.applicationGuildCommands( CLIENT_ID, DEV_GUILD_ID ), { body: commands } )
-                .then( () => { console.log( chalk.green( 'Successfully cleared developer guild slash commands.' ) ); } )
-                .catch( errPutRest => { console.error( chalk.red( `Failed to clear developer guild slash commands:\n${errPutRest.stack}` ) ); } );
-        }
-    } catch ( noRest ) { console.error( chalk.red( `Failed to initiate rest:\n${noRest}` ) ); }
-    console.log( 'YOU CAN NOW STOP THE INSTANCE AND UPDATE THE STARTUP COMMAND TO `node index.js`/' );
-    setTimeout( () => { console.log( 'TIME IS UP!' ); }, 3600000 );
+      console.log( chalk.yellow( 'Attempting to clear global slash commands...' ) );
+      await rest.put( Routes.applicationCommands( CLIENT_ID ), { body: commands } )
+        .then( () => { console.log( chalk.green( 'Successfully cleared global slash commands.' ) ); } )
+        .catch( errPutRest => { console.error( chalk.red( `Failed to clear global slash commands:\n${errPutRest.stack}` ) ); } );
+
+      console.log( chalk.yellow( 'Attempting to clear developer guild slash commands...' ) );
+      await rest.put( Routes.applicationGuildCommands( CLIENT_ID, DEV_GUILD_ID ), { body: commands } )
+        .then( () => { console.log( chalk.green( 'Successfully cleared developer guild slash commands.' ) ); } )
+        .catch( errPutRest => { console.error( chalk.red( `Failed to clear developer guild slash commands:\n${errPutRest.stack}` ) ); } );
+
+  }
+  catch ( noRest ) { console.error( chalk.red( `Failed to initiate rest:\n${noRest}` ) ); }
+  console.log( 'YOU CAN NOW STOP THE INSTANCE AND UPDATE THE STARTUP COMMAND TO `index.js`' );
+  setTimeout( () => { console.log( 'TIME IS UP!' ); }, 600000 );
+  setTimeout( () => { console.log( 'RESTARTING!!!' ); }, 900000 );
 } )();
