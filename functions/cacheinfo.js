04908c8a4f26298d8e43ba59ae63906346d26b34
Donald J. Fortier II <Technical_13@yahoo.com>
2026-03-30 09:25:15 -0400

    Use svg icon's parent anchor for cache type
    Replace selector that read the cache type from 'a.cacheImage' with one that finds the 'svg.icon.cache-icon' and reads its parent anchor's title. This makes the parser robust to markup changes where the icon SVG is nested inside the anchor element.

--- functions/cacheinfo.js
+++ functions/cacheinfo.js
@@ -85,7 +85,7 @@ module.exports = async ( gcCode ) => {
           size: $( '#ctl00_ContentBody_size > p > span > small' ).text().trim().replace( /[\(\)]/g, '' ),
           state: state,
           terrain: $( '#ctl00_ContentBody_Localize12 > img' ).attr( 'alt' ).split( ' ' )[ 0 ],
-          type: $( 'a.cacheImage' ).attr( 'title' ).trim()
+          type: $( 'svg.icon.cache-icon' ).parents( 'a' ).attr( 'title' ).trim()
         };
       }
       return result;
