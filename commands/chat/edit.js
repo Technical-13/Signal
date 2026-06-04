d2ef17c485258ec1722e4f19aca86d763a26eb57
Donald J. Fortier II <Technical_13@yahoo.com>
2026-06-04 16:45:12 -0400

    Improve §edit command validation and safety
    Refactor the chat edit command to add stricter validation and safer handling. Rename the incoming message variable to `msg` and preserve raw args; accept full Discord message links and normal snowflake IDs, validating IDs via BigInt/timestamp checks. Introduce an abort flow for invalid input, avoid continuing when required args are missing, and ensure replies consistently use `msg`. Harden logging and edits with optional chaining/fallbacks for empty content, improve permission/mention checks, and tidy up error/log handling.

--- commands/chat/edit.js
+++ commands/chat/edit.js
@@ -11,60 +11,72 @@ module.exports = {
   group: 'chat',
   description: 'Edit a bot message.\n\t§edit [message.id] [new message]',
   cooldown: 1000,
-	run: async ( client, message, args ) => {
+	run: async ( client, msg, args ) => {
     try {
-      message.delete();
-      var delResponse;
-      if ( args.length === 0 || !( /[\d]{17,19}/.test( args[ 0 ] ) ) ) { delResponse = await message.reply( { content: 'You forgot to tell me what message to edit.' } ); }
-      else if ( args.length === 1 && ( /[\d]{17,19}/.test( args[ 0 ] ) ) ) { delResponse = await message.reply( { content: 'You forgot to tell me what the new message should be.' } ); }
-      const { channel, guild, author } = message;
+      msg.delete();
+      const rawArgs = [ ...args ];
+      var delResponse, abortReply = false;
+      const msgLink = /https?:\/\/(?:[a-z]+\.)?discord\.com\/channels\/\d{17,19}\/\d{17,19}\/(\d{17,19})/i;
+      const isValidTarget = args.length >= 1 ? ( /^\d{17,19}$/.test( args[ 0 ] ) || msgLink.test( args[ 0 ] ) ) : false;
+      if ( args.length === 0 || !isValidTarget ) { delResponse = await msg.reply( { content: 'You forgot to tell me what message to edit.' } ); abortReply = true; }
+      else if ( args.length === 1 && isValidTarget ) { delResponse = await msg.reply( { content: 'You forgot to tell me what the new message should be.' } ); abortReply = true; }
+      const { author, channel, guild } = msg;
       const guildMember = await guild.members.cache.get( author.id );
       const { isBotMod, checkPermission, guildAllowsPremium, isServerBooster, isWhitelisted, content } = await userPerms( author, guild );
-      if ( content ) { return message.reply( { content: content } ); }
+      if ( content ) { return msg.reply( { content: content } ); }
 
       const canSpeak = ( isBotMod || checkPermission( 'ManageGuild' ) ? true : false );
-      const msgID = args.shift();
+      let msgID = abortReply ? args?.[ 0 ] || '' : args.shift();
+      if ( msgLink.test( msgID ) ) { msgID = msgID.match( msgLink )[ 1 ]; }
+      if ( typeof msgID === 'string' && /^\d{17,19}$/.test( msgID ) ) {
+        const bigMsgID = BigInt( msgID ) >> 22n, bigEpoch = BigInt( Date.UTC( 2015, 0, 1 ) ), bigNow = BigInt( Date.now() );
+        if ( !( bigMsgID > 0n && ( bigMsgID + bigEpoch ) <= bigNow ) ) {
+          console.error( 'Error in %s:\n\tUnable to parse `msgID` from %s', strScript, rawArgs[ 0 ] );
+          delResponse = await msg.reply( { content: 'You forgot to tell me what message to edit.' } );
+          abortReply = true;
+        }
+      }
       const mySaying = args.join( ' ' );
-      const mentionsEveryone = /@(everyone|here)/g.test( mySaying );
+      const mentionsEveryone = /@(everyone|here)/g.test( mySaying ) || false;
       const strEveryoneHere = ( mentionsEveryone ? '`@' + ( /@everyone/g.test( mySaying ) ? 'everyone' : 'here' ) + '`' : null );
       const strAuthorTag = author.tag;
 
       const { doLogs, chanChat, strClosing } = await getGuildConfig( guild );
       if ( !chanChat ) { doLogs = false; console.error( 'Error in %s:\n\tVariable `chanChat` is %s', strScript, chanChat ); }
-      if ( mySaying ) {
+      if ( !strClosing ) { doLogs = false; console.error( 'Error in %s:\n\tVariable `strClosing` is %s', strScript, strClosing ); }
+      if ( mySaying && !abortReply ) {
         const parsedSaying = await parse( mySaying, { member: guildMember } );
-        if ( canSpeak && ( !mentionsEveryone || checkPermission( 'MentionEveryone' ) ) ) {
-          channel.messages.fetch( msgID ).then( async message => {
-            let oldContent = message.content;
-            await message.edit( { content: parsedSaying } ).then( async edited => {
+        channel.messages.fetch( msgID ).then( async msg => {
+          let oldContent = msg?.content || 'empty';
+          if ( canSpeak && ( !mentionsEveryone || checkPermission( 'MentionEveryone' ) ) ) {
+            await msg.edit( { content: parsedSaying } ).then( async edited => {
               if ( doLogs ) {
                 chanChat.send( { content:
-                  'I edited what I said in https://discord.com/channels/' + edited.guild.id + '/' + edited.channel.id + '/' + edited.id + ' at <@' + author.id + '>\'s request from:\n```\n' + oldContent + '\n```\nTo:\n```\n' + edited.content + '\n```' + strClosing
+                  'I edited what I said in https://discord.com/channels/' + edited.guild.id + '/' + edited.channel.id + '/' + edited.id + ' at <@' + author.id + '>\'s request from:\n```\n' + oldContent + '\n```\nTo:\n```\n' + ( edited?.content || 'empty' ) + '\n```' + strClosing
                 } )
-                .catch( async noLogChan => { delResponse = await message.reply( await errHandler( noLogChan, { chanType: 'chat', command: 'edit', channel: channel, type: 'logLogs' } ) ); } );
+                .catch( async noLogChan => { delResponse = await msg.reply( await errHandler( noLogChan, { chanType: 'chat', command: 'edit', channel: channel, type: 'logLogs' } ) ); } );
               }
-              delResponse = await message.reply( { content: 'I edited my message for you!' } );
+              delResponse = await msg.reply( { content: 'I edited my message for you!' } );
             } )
-            .catch( async errSend => { delResponse = await message.reply( await errHandler( errSend, { command: 'edit', guild: guild, type: 'errSend' } ) ); } );
-          } )
-          .catch( async errFetch => { delResponse = await message.reply( await errHandler( errFetch, { command: 'edit', msgID: msgID, type: 'errFetch' } ) ); } );
-        }
-        else if ( mentionsEveryone && !checkPermission( 'MentionEveryone' ) ) {
-          if ( doLogs ) {
-            chanChat.send( { content: '<@' + author.id + '> has no permission to get me to ' + strEveryoneHere + ' in <#' + channel.id + '>. They tried to get me to change my message from:\n```\n' + oldContent + '\n```\nTo:\n```\n' + edited.content + '\n```' + strClosing } )
-            .catch( async noLogChan => { delResponse = await message.reply( await errHandler( noLogChan, { chanType: 'chat', command: 'edit', channel: channel, type: 'logLogs' } ) ); } );
+            .catch( async errSend => { delResponse = await msg.reply( await errHandler( errSend, { command: 'edit', guild: guild, type: 'errSend' } ) ); } );
           }
-          delResponse = await message.reply( { content: 'You have no permission to get me to ' + strEveryoneHere + ' in <#' + channel.id + '>!' } );
-        }
-        else {
-          if ( doLogs ) {
-            chanChat.send( { content:  '<@' + author.id + '> has no permission to use my `/edit` command from <#' + channel.id + '>. They tried to get me to change my message from:\n```\n' + oldContent + '\n```\nTo:\n```\n' + edited.content + '\n```' + strClosing } )
-            .catch( async noLogChan => { delResponse = await message.reply( await errHandler( noLogChan, { chanType: 'chat', command: 'edit', channel: channel, type: 'logLogs' } ) ); } );
+          else if ( mentionsEveryone && !checkPermission( 'MentionEveryone' ) ) {
+            if ( doLogs ) {
+              chanChat.send( { content: '<@' + author.id + '> has no permission to get me to ' + strEveryoneHere + ' in <#' + channel.id + '>. They tried to get me to change my message from:\n```\n' + oldContent + '\n```\nTo:\n```\n' + parsedSaying + '\n```' + strClosing } )
+              .catch( async noLogChan => { delResponse = await msg.reply( await errHandler( noLogChan, { chanType: 'chat', command: 'edit', channel: channel, type: 'logLogs' } ) ); } );
+            }
+            delResponse = await msg.reply( { content: 'You have no permission to get me to ' + strEveryoneHere + ' in <#' + channel.id + '>!' } );
           }
-          delResponse = await message.reply( { content: 'You have no permission to use my `/edit` command in <#' + channel.id + '>!' } );
-        }
+          else {
+            if ( doLogs ) {
+              chanChat.send( { content:  '<@' + author.id + '> has no permission to use my `/edit` command from <#' + channel.id + '>. They tried to get me to change my message from:\n```\n' + oldContent + '\n```\nTo:\n```\n' + parsedSaying + '\n```' + strClosing } )
+              .catch( async noLogChan => { delResponse = await msg.reply( await errHandler( noLogChan, { chanType: 'chat', command: 'edit', channel: channel, type: 'logLogs' } ) ); } );
+            }
+            delResponse = await msg.reply( { content: 'You have no permission to use my `/edit` command in <#' + channel.id + '>!' } );
+          }
+        } ).catch( async errFetch => { delResponse = await msg.reply( await errHandler( errFetch, { command: 'edit', msgID: msgID, type: 'errFetch' } ) ); } );
       }
-      else { delResponse = await message.reply( { content: 'I don\'t know what to say.' } ); }
+      else if ( !abortReply ) { delResponse = await msg.reply( { content: 'I don\'t know what to say.' } ); }
       if ( delResponse ) { setTimeout( () => { delResponse.delete(); }, 300000 ); }
     }
     catch ( errObject ) { console.error( 'Uncaught error in %s:\n\t%s', strScript, errObject.stack ); }
